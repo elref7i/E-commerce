c265dbb1df121404d8a0fa80d8b01f74e98bc23e
Ahmed Refai <momo3956248@gmail.com>
2024-11-25 19:29:50 +0200

    Design Footer not Responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,80 @@
+import amzonePay from '../../assets/images/amazon-pay.png';
+import masrerCard from '../../assets/images/mastercard.webp';
+import payPal from '../../assets/images/paypal.png';
+import americanExpress from '../../assets/images/American-Express-Color.png';
+import getGooglePlay from '../../assets/images/get-google-play.png';
+import getAppleStore from '../../assets/images/get-apple-store.png';
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-8 shadow-md">
+        <div className="container">
+          <header className="mb-6 space-y-2">
+            <h3 className="text-xl">Get the FreshCart app</h3>
+            <p className="text-slate-400">
+              We will send you a link, open it on Your phone to download the
+              app.
+            </p>
+          </header>
+          <form className="contact-email  grid grid-cols-12 gap-2 relative before:absolute before:w-full before:h-[2px] before:bg-gray-200 before:-bottom-8  mb-5 ">
+            <div className="email-input col-span-10">
+              <input
+                type="email"
+                className="form-controll"
+                placeholder="Email.."
+              />
+            </div>
+            <button
+              className="btn  bg-primary-500 hover:bg-primary-600"
+              type="submit"
+            >
+              Share App Link
+            </button>
+          </form>
+          <div className="py-12 flex gap-3 items-center justify-between">
+            <div className="payment flex gap-3 items-center">
+              <h4>Payment Pathers</h4>
+              <ul className="payment-way flex gap-3 items-center">
+                <li className="w-8">
+                  <a href="#" target="_blank" alt="">
+                    <img className="w-full" src={amzonePay} alt="" />
+                  </a>
+                </li>
+                <li className="w-8">
+                  <a href="#" target="_blank" alt="">
+                    <img src={americanExpress} alt="" />
+                  </a>
+                </li>
+                <li className="w-8">
+                  <a href="#" target="_blank" alt="">
+                    <img src={masrerCard} alt="" />
+                  </a>
+                </li>
+                <li className="w-8">
+                  <a href="#" target="_blank" alt="">
+                    <img src={payPal} alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="download-way flex gap-3 items-center">
+              <h4>Get devliveries with FreshCart</h4>
+              <ul className="payment-way flex gap-3 items-center  ">
+                <li className="w-24">
+                  <a href="#" target="_blank" alt="">
+                    <img className="w-full" src={getAppleStore} alt="" />
+                  </a>
+                </li>
+                <li className="w-24">
+                  <a href="#" target="_blank" alt="">
+                    <img className="w-full" src={getGooglePlay} alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
