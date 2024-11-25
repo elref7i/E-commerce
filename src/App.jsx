771a8be4b532b4ff3c0e5e3d748b25c1fa7ac9b9
Ahmed Refai <momo3956248@gmail.com>
2024-11-25 03:07:13 +0200

    Add font default,  primary color and add compontent layout (Routing)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import Home from './Pages/Home/Home';
+import Signup from './Pages/Signup/Signup';
+import Login from './Pages/Login/Login';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/home', element: <Home /> },
+      { path: '/signup', element: <Signup /> },
+      { path: '/login', element: <Login /> },
+    ],
+  },
+]);
 export default function App() {
   return (
     <>
-      <div className="bg-blue-800 text-center px-12">
-        <i className="fa-brands fa-facebook fa"></i>dsd
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
