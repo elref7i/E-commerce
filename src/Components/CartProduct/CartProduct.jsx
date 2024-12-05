44efe85c4dd6f657b33ad676034ae5c8aecbfb7c
Ahmed Refai <momo3956248@gmail.com>
2024-12-05 20:15:38 +0200

    Finsih add and get product

--- src/Components/CartProduct/CartProduct.jsx
+++ src/Components/CartProduct/CartProduct.jsx
@@ -1,49 +1,37 @@
 /* eslint-disable react/prop-types */
 export default function CartProduct({ cartInfo }) {
   console.log(cartInfo);
-  const { category, title, imageCover } = cartInfo.product;
+  const { price, count } = cartInfo;
+  const { category, title, imageCover, id } = cartInfo.product;
 
   return (
     <>
-      <div className="flex items-center gap-0 sm:gap-5  justify-center sm:justify-between shadow-md p-3 flex-wrap relative">
-        <div className="flex gap-5 items-center ">
-          <div className="">
+      <div className="grid grid-cols-12 items-center gap-3   justify-center sm:justify-between shadow-md p-1  relative">
+        <div className="flex gap-5 items-center  col-span-12 sm:col-span-9 ">
+          <div className=" bg-black">
             <img
               src={imageCover}
-              className="w-[150px] h-full object-cover"
+              className="w-[150px] h-[150px] object-cover"
               alt=""
             />
           </div>
           <article>
-            <h2 className="text-nowrap text-lg font-semibold text-primary-500">
-              Ahmed khaled refai
+            <h2 className="text-sm md:text-lg font-semibold text-primary-500">
+              {title}
             </h2>
-            <h3 className="">Labtop</h3>
-            <h4 className="text-primary-300">$1117</h4>
+            <h3 className="text-[12px] sm:text-sm">{category.name}</h3>
+            <h4 className="text-primary-300 font-semibold">${price}</h4>
           </article>
         </div>
-        <div className="addtion  flex justify-center gap-5 items-center bg-slate-200 p-2 rounded-md">
+        <div className="addtion  flex justify-center gap-5 items-center bg-slate-200 w-fit justify-self-center p-2 rounded-md col-span-12 sm:col-span-3">
           <i className="fa-solid fa-minus text-lg font-bold text-primary-500 hover:text-primary-700 duration-300 transition-colors cursor-pointer"></i>
-          <div className="counter text-xl font-bold text">1</div>
+          <div className="counter text-xl font-bold text">{count}</div>
           <i className="fa-solid fa-plus text-lg font-bold text-primary-500 hover:text-primary-700 duration-300 transition-colors cursor-pointer"></i>
         </div>
-        <div className="size-8 absolute top-0 right-0 m-3 bg-slate-200 text-red-600  flex rounded-full items-center justify-center hover:text-slate-200 hover:bg-red-600 duration-200 transition-colors">
+        <div className="size-8 absolute top-2 right-1  bg-slate-200 text-red-600  flex rounded-full items-center justify-center hover:text-slate-200 hover:bg-red-600 duration-200 transition-colors">
           <i className="fa-solid fa-x text-lg cursor-pointer "></i>
         </div>
       </div>
     </>
   );
-}
-
-{
-  /* <div className="w-[200px]">
-        </div>
-        <h2 className="text-lg">Ahmed khaled Refai</h2>
-   
-        <div className="addtion  flex justify-center gap-5 items-center ">
-          <i className="fa-solid fa-minus text-lg font-bold text-primary-500 hover:text-primary-700 duration-300 transition-colors cursor-pointer"></i>
-          <div className="counter text-xl font-bold text">1</div>
-          <i className="fa-solid fa-plus text-lg font-bold text-primary-500 hover:text-primary-700 duration-300 transition-colors cursor-pointer"></i>
-        </div>
-        <i className="fa-solid fa-x text-red-600 text-lg cursor-pointer"></i> */
 }
--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -6,7 +6,6 @@ import CartProduct from '../../Components/CartProduct/CartProduct';
 
 export default function Cart() {
   const { cartInfo, getProductToCart } = useContext(CartContext);
-
   useEffect(() => {
     getProductToCart();
   }, []);
@@ -26,9 +25,17 @@ export default function Cart() {
               ))}
             </div>
           )}
-          <button className="clea btn bg-red-600 text-xl font-medium block ml-auto hover:bg-red-700 px-4 py-3">
-            Clear All
-          </button>
+          <div className="flex justify-center sm:justify-between items-center flex-wrap gap-3">
+            <p className="text-lg">
+              Your Total Cart Price{' '}
+              <span className="font-bold text-primary-500">
+                ${cartInfo.data.totalCartPrice}
+              </span>
+            </p>
+            <button className="clea btn bg-red-600 text-xl font-medium block ml-auto hover:bg-red-700 px-4 py-3">
+              Clear All
+            </button>
+          </div>
         </section>
       )}
     </>
