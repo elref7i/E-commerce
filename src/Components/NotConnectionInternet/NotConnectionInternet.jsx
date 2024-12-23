c08c454d71fcdf11ffe1620be63a02a079ce2e0b
Ahmed Refai <momo3956248@gmail.com>
2024-12-23 07:51:02 +0200

    add component check logout user

--- src/Components/NotConnectionInternet/NotConnectionInternet.jsx
+++
@@ -1,9 +0,0 @@
-// // import notConection from '../../assets/images/notConection.png';
-
-// export default function NotConnectionInternet() {
-//   return (
-//     <div className=" fixed bottom-0 text-primary-500 h-screen text-center font-bold bg-slate-200 ">
-//       <p>Not Connection with internet</p>
-//     </div>
-//   );
-// }
